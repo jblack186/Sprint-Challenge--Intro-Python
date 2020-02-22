102e4f43510223118934f9d42385ab067786ceab
Jamison Blackwell <blackwellj1040@gmail.com>
2020-02-21 20:59:33 -0500

    finished

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -23,31 +23,46 @@ humans = [
 
 # Write a list comprehension that creates a list of names of everyone
 # whose name starts with 'D':
-print("Starts with D:")
+print(f"Starts with D:")
+
 a = []
+for per in humans:
+    if per.name[0] == "D":
+        a.append(per.name)
 print(a)
 
 # Write a list comprehension that creates a list of names of everyone
 # whose name ends in "e".
 print("Ends with e:")
 b = []
+for per in humans:
+    if per.name[-1] == "e":
+        b.append(per.name)
 print(b)
 
 # Write a list comprehension that creates a list of names of everyone
 # whose name starts with any letter between 'C' and 'G' inclusive.
 print("Starts between C and G, inclusive:")
 c = []
+for per in humans:
+    if per.name[0] == "C" or per.name[0] == "D" or per.name[0] == "E" or per.name[0] == "F" or per.name[0] == "G":
+        c.append(per.name)
 print(c)
 
 # Write a list comprehension that creates a list of all the ages plus 10.
 print("Ages plus 10:")
 d = []
+
+for per in humans:
+    d.append(per.age + 10)
 print(d)
 
 # Write a list comprehension that creates a list of strings which are the name
 # joined to the age with a hyphen, for example "David-31", for all humans.
 print("Name hyphen age:")
 e = []
+for per in humans:
+    e.append(f'{per.name}-{per.age}')
 print(e)
 
 # Write a list comprehension that creates a list of tuples containing name and
@@ -55,17 +70,25 @@ print(e)
 # inclusive.
 print("Names and ages between 27 and 32:")
 f = []
+for per in humans:
+    if per.age >= 27 and per.age <= 32:
+        f.append((per.name, per.age))
+
 print(f)
 
 # Write a list comprehension that creates a list of new Humans like the old
 # list, except with all the names uppercase and the ages with 5 added to them.
 # The "humans" list should be unmodified.
+import copy
 print("All names uppercase:")
-g = []
+g = [Human(x.name.upper(), x.age + 5) for x in humans]
 print(g)
 
 # Write a list comprehension that contains the square root of all the ages.
 print("Square root of ages:")
 import math
 h = []
+for per in humans:
+    h.append(math.sqrt(per.age))
+
 print(h)
--- src/oop/oop2.py
+++ src/oop/oop2.py
@@ -4,10 +4,21 @@
 # object is constructed.
 
 class GroundVehicle():
-    def __init__(self, num_wheels):
+    def __init__(self, num_wheels=4):
         self.num_wheels = num_wheels
 
+    def drive(self):
+        return "vroooom"
+        
     # TODO
+# print(GroundVehicle.drive())
+
+class Motorcycle(GroundVehicle):
+    def __init__(self, num_wheels=2):
+        super().__init__(num_wheels)
+
+    def drive(self):
+        return "BRAAAP!!"
 
 
 # Subclass Motorcycle from GroundVehicle.
@@ -26,7 +37,8 @@ vehicles = [
     GroundVehicle(),
     Motorcycle(),
 ]
-
+for car in vehicles:
+    print(car.drive())
 # Go through the vehicles list and print the result of calling drive() on each.
 
 # TODO
